120326305d44c2f2562c89e5173c0775c25bd1e7
Iohiaeri <102757430+Iohiaeri@users.noreply.github.com>
2022-04-22 14:24:40 -0700

    Add files via upload

---
+++ Prompt5.py
@@ -0,0 +1,16 @@
+import numpy as np
+from tabulate import tabulate
+
+def main():
+    x=0
+    values = ({'x':[],'sin(x)':[]})
+    for i in range(1001):
+        y=np.sin(x)
+        values["x"].append(x)
+        values["sin(x)"].append(y)
+        x += ((2*np.pi)/1000)
+    print(tabulate(values, headers = "keys"))
+if __name__ == "__main__":
+    main()
+    
+
