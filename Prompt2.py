9c2c9a44e41cd7e929fdaa772ddb13f5281db001
Iohiaeri <102757430+Iohiaeri@users.noreply.github.com>
2022-04-21 20:59:47 -0700

    Add files via upload

---
+++ Prompt2.py
@@ -0,0 +1,11 @@
+def main():
+    sums = (15.3+17.8)
+    print(sums, type(sums))
+    difference = (15-17)
+    print(difference, type(difference))
+    product = (15.3*17)
+    print(product, type(product))
+    
+
+if __name__=="__main__":
+    main()
