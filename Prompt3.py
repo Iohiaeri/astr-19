400c85c6fa76a6f30dc803f53e953dcb35f13937
Iohiaeri <102757430+Iohiaeri@users.noreply.github.com>
2022-04-21 21:10:37 -0700

    Add files via upload

---
+++ Prompt3.py
@@ -0,0 +1,12 @@
+def f(x):
+    results = x**3+8
+    return results
+def main():
+    x=9
+    if f(x) > 27:
+        print("YAY!")
+    else:
+        print("OOPS!")
+if __name__ =="__main__":
+    main()
+
